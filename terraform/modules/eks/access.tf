7937888dc31ad908f32131723bb2ea5ba7de165b
ShaiMullo <shaimullokandov@gmail.com>
2026-01-31 20:42:32 +0200

    fix: remove redundant eks access entry for root

--- terraform/modules/eks/access.tf
+++ terraform/modules/eks/access.tf
@@ -1,16 +1,1 @@
-# Emergency Access for Root User
-resource "aws_eks_access_entry" "root" {
-  cluster_name  = module.eks.cluster_name
-  principal_arn = "arn:aws:iam::268456953512:root"
-  type          = "STANDARD"
-}
-
-resource "aws_eks_access_policy_association" "root" {
-  cluster_name  = module.eks.cluster_name
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = aws_eks_access_entry.root.principal_arn
-
-  access_scope {
-    type = "cluster"
-  }
-}
+# Root access is handled by enable_cluster_creator_admin_permissions = true in main.tf
